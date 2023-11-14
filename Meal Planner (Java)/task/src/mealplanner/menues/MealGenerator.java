833cf5f69a8c6279a5017750418489c6b88168a9
Theo Higgins <higgins.theo1@gmail.com>
2023-11-13 19:48:51 -0500

    Staged 2 Completed

--- Meal Planner (Java)/task/src/mealplanner/menues/MealGenerator.java
+++ Meal Planner (Java)/task/src/mealplanner/menues/MealGenerator.java
@@ -4,6 +4,8 @@ import mealplanner.model.Meal;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MealGenerator {
     private static final String MEALPROMPT = "Which meal do you want to add" +
@@ -18,40 +20,78 @@ public class MealGenerator {
         this.scanner = new Scanner(System.in);
     }
 
-    public void start() {
+    public Meal start() {
         meal = new Meal();
         setMealtype();
         setMealName();
         setIngredients();
-        System.out.println(meal);
+        return meal;
 
     }
 
     public void setMealtype() {
         String input = "";
-
+        System.out.println(MEALPROMPT);
         while (true) {
-            System.out.println(MEALPROMPT);
             input = scanner.nextLine();
             if (input.equals("breakfast") ||
                     input.equals("lunch") ||
                     input.equals("dinner")) {
+                meal.setCategory(input);
                 break;
+            } else {
+                System.out.println("Wrong meal category! Choose from: breakfast, lunch, dinner.");
             }
         }
-
-        meal.setCategory(input);
-
     }
 
     public void setMealName() {
+        String input = "";
         System.out.println(MEALNAMEPROMPT);
-        meal.setName(scanner.nextLine());
+
+        while (true) {
+            input = scanner.nextLine();
+            if (!input.isBlank() && isValidInput(input)) {
+                meal.setName(input);
+                break;
+            }
+            System.out.println("Wrong format. Use letters only!");
+        }
+    }
+
+    private boolean isValidInput(String input) {
+        String sub = input.substring(input.length() - 2);
+
+        if (", ".equals(sub)) {
+            return false;
+        }
+
+        Pattern pattern = Pattern.compile("^[^\\d\\W][\\W\\D\\s]*[^ \\d\\W]$");
+        List<String> ingredients = List.of(input.split(", "));
+        Boolean validEntries = ingredients.stream().allMatch(e -> {
+            Matcher matcher = pattern.matcher(e);
+            return matcher.find();
+        });
+
+        return validEntries;
+
     }
 
     public void setIngredients() {
+        String input = "";
         System.out.println(INGREDIENTSPROMPT);
-        meal.setIngredients(List.of(scanner.nextLine().split(",")));
+
+        while (true) {
+            input = scanner.nextLine();
+
+            if (!input.isBlank() && isValidInput(input)) {
+                meal.setIngredients(List.of(input.split(",")));
+                break;
+            } else {
+                System.out.println("Wrong format. Use letters only!");
+            }
+        }
+
     }
 
 
